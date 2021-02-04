8f0abe9655a84042bfe49eff78dcc0b3732db5b5
skhattab <ksm73@pitt.edu>
2021-02-04 12:28:48 -0500

    correcting BinaryTreeInterface

--- TreeADT/BinaryTreeInterface.java
+++ TreeADT/BinaryTreeInterface.java
@@ -1,4 +1,4 @@
-public interface BinaryTreeInterface<T> {
+public interface BinaryTreeInterface<T> extends TreeInterface<T>{
   public void buildTree(T rootData);
   public void buildTree(T rootData,
                         BinaryTreeInterface<T> left,
